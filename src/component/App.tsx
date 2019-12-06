6835650a0e757c65719c316f6165a38e5cf1e709
alvin.zhou <alvin.zhou@ximalaya.com>
2019-12-06 16:32:01 +0800

    test with cat ui component

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Avatar } from '@hife/catui';
 
 interface Props {
   name: string;
@@ -6,6 +7,12 @@ interface Props {
 
 export default class App extends Component<Props> {
   render() {
-    return <div>this is {this.props.name}</div>;
+    return <div>this is {this.props.name}
+      <div>
+        <Avatar
+          src='https://raw.githubusercontent.com/sialvsic/static-host/master/public/laptop.png'
+          size='large'
+        />
+      </div></div>;
   }
 }
